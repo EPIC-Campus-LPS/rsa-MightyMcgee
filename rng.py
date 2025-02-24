92370a55da0997c6d12f71710df548086bb92fa7
Sean Major <143544671+MightyMcgee@users.noreply.github.com>
2025-02-24 11:00:34 -0700

    Add files via upload

---
+++ rng.py
@@ -0,0 +1,21 @@
+def rnum(upper,lower):
+    import cv2
+    from blake3 import blake3
+
+    diff = upper - lower
+
+    camera = cv2.VideoCapture(0)
+    ret, frame = camera.read()
+    cv2.imwrite("captured_image.jpg", frame)
+    camera.release()
+
+    for i in range(100):
+        result, image = camera.read()
+
+    with open('captured_image.jpg', 'rb') as file:
+        image_bytes = file.read()
+    image_hash = blake3(image_bytes).hexdigest()
+    i = int(image_hash, 16)
+    i = i % diff
+    i = i + lower
+    return(i)
